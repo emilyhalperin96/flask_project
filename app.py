520c378619642808f877a761053f32fda1a57ce9
emilyphalperin@gmail.com <emilyhalperin@Emilys-MBP-2.lan>
2023-10-20 16:48:54 -0400

    combine endpoints

--- app.py
+++ app.py
@@ -20,20 +20,18 @@ def post(post_id):
         return render_template('404.html', message = f"A post with id {post_id} was not found.")
     return render_template('post.html', post=post)
 
-@app.route('/post/form')
-def form():
-    return render_template('create.jinja2')
-
-@app.route('/post/create', methods=['POST'])
+@app.route('/post/create', methods=['GET','POST'])
 def create_post():
-    title = request.form.get('title')
-    content = request.form.get('content')
-    post_id = len(posts)
-    posts[post_id] = {'id': post_id, 'title': title, 'content': content}
+    if request.method == 'POST':
+        title = request.form.get('title')
+        content = request.form.get('content')
+        post_id = len(posts)
+        posts[post_id] = {'id': post_id, 'title': title, 'content': content}
 
-    #takes in function name and returns an address that we want 
-    #takes the post function and returns the url 
-    return redirect(url_for('post', post_id=post_id))
+        #takes in function name and returns an address that we want 
+        #takes the post function and returns the url 
+        return redirect(url_for('post', post_id=post_id))
+    return render_template('create.jinja2')
 
 if __name__ == '__main__':
     app.run(debug=True)
